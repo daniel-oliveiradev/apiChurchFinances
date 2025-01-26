c7a0addacf9056efd3fd4e521b557576296ce824
Daniel Oliveira <daniel.oliveiraprog@gmail.com>
2025-01-25 21:49:52 -0300

    update prisma schema(table user)

---
+++ prisma/migrations/20250126004815_add_validation_id_and_is_validated_in_the_table_user/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_user" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "nickname" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "validationId" TEXT,
+    "isValidated" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_user" ("createdAt", "email", "id", "isAdmin", "name", "nickname", "password", "updatedAt") SELECT "createdAt", "email", "id", "isAdmin", "name", "nickname", "password", "updatedAt" FROM "user";
+DROP TABLE "user";
+ALTER TABLE "new_user" RENAME TO "user";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
