069be06ea31b64b32e8cbfb1b7f7e32ea094add3
Daniel Oliveira <daniel.oliveiraprog@gmail.com>
2025-02-17 09:17:16 -0300

    fastify cors install and config

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import fastify from "fastify";
 import { routes } from "./routes";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify()
 const PORT = 3000
+const cors = fastifyCors
+
+app.register(cors, {})
+app.register(routes)
 
 app.listen({
   port: PORT
 }, () => console.log(`HTTP server running on port ${PORT}`))
-
-app.register(routes)
